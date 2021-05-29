7fbb4e1d3970fbaa8dec1c1afc7c8eb5efd0593e
sulynajimsj <83905043+sulynajimsj@users.noreply.github.com>
2021-05-29 19:37:46 +0400

    Uploaded all the Python files
    All files needed to make the space games were uploaded

---
+++ Welcome.py
@@ -0,0 +1,36 @@
+import pygame
+import sys
+
+
+pygame.init()
+res = (700, 700)
+screen = pygame.display.set_mode(res)
+width = screen.get_width()
+height = screen.get_height()
+
+# defining a font
+bigfont = pygame.font.SysFont('Anima', 70)
+smallfont = pygame.font.SysFont('Segoe UI', 20)
+text = smallfont.render('Left Arrow for Space Invader', True, (250,250,250))
+text2 = smallfont.render('Right Arrow for Space PVP (NEW!)', True, (250,250,250))
+text3 = bigfont.render("Space Arcade", True, (250,250,250))
+
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                exec(open('main.py').read())
+            if event.key == pygame.K_RIGHT:
+                exec(open('main2.py').read())
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+
+
+    screen.fill((60, 25, 60))
+    screen.blit(text, (200, height / 2))
+    screen.blit(text2, (200, height / 2+100))
+    screen.blit(text3, (200, 50))
+    pygame.display.update()
